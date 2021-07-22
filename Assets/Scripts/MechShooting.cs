446ecfecec5b2c106abdaabc478032b35b3fb7cc
beanoh <sam.beahan@gmail.com>
2021-07-22 15:50:59 +0930

    updated mech firing

--- Assets/Scripts/MechShooting.cs
+++ Assets/Scripts/MechShooting.cs
@@ -12,12 +12,16 @@ public class MechShooting : MonoBehaviour
     private List<GameObject> guns;
     private int mode;
     private float timer;
+    private bool l_fired;
+    private bool r_fired;
 
     // Start is called before the first frame update
     void Start()
     {
-        mode = Random.Range(1, 4);
-        timer = -1;
+        mode = 1;
+        timer = 0;
+        l_fired = false;
+        r_fired = false;
         gun_to_tar = new Dictionary<GameObject, Vector3>
         {
             { mg, new Vector3(0, 0, 0) },
@@ -34,40 +38,56 @@ public class MechShooting : MonoBehaviour
         foreach (GameObject gun in guns)
         {
             gun_to_tar[gun] = mm.player_v + (mm.player.GetComponent<Rigidbody>().velocity * (mm.player_v.magnitude / gun.GetComponent<Gun>().bulletSpeed));
-            
+
         }
         if (mm.withinRange)
         {
             timer += Time.deltaTime;
             if (mode == 1)
             {
-                if (timer > 0)
-                {
-                    print("shooting left");
-                    head.transform.LookAt(transform.position + gun_to_tar[rl_1]);
-                    rl_1.GetComponent<Gun>().shoot();
-                }
-                if (timer > 0.5)
-                {
-                    head.transform.LookAt(transform.position + gun_to_tar[rl_2]);
-                    rl_2.GetComponent<Gun>().shoot();
-                }
-                if (timer > 5)
+                head.transform.LookAt(transform.position + gun_to_tar[mg]);
+                mg.GetComponent<Gun>().shoot();
+                if (timer > 20)
                 {
                     timer = 0;
                     mode = 2;
+                    mg.GetComponent<Gun>().reload();
                 }
+
             }
             if (mode == 2)
             {
-                head.transform.LookAt(transform.position + gun_to_tar[mg]);
-                mg.GetComponent<Gun>().shoot();
-                if (timer > 5)
+                if (timer > 1)
+                {
+                    if (l_fired == false)
+                    {
+                        print("shooting left");
+                        head.transform.LookAt(transform.position + gun_to_tar[rl_1]);
+                        rl_1.GetComponent<Gun>().shoot();
+                        l_fired = true;
+                    }
+                    
+                }
+                if (timer > 2)
+                {
+                    if (r_fired == false)
+                    {
+                        print("shooting left");
+                        head.transform.LookAt(transform.position + gun_to_tar[rl_2]);
+                        rl_2.GetComponent<Gun>().shoot();
+                        r_fired = true;
+                    }
+                }
+                if (timer > 3)
                 {
                     timer = 0;
-                    mode = 3;
+                    mode = 1;
+                    rl_1.GetComponent<Gun>().reload();
+                    rl_2.GetComponent<Gun>().reload();
+                    r_fired = false;
                 }
             }
+
             if (mode == 3)
             {
                 head.transform.LookAt(transform.position + gun_to_tar[blaster]);
@@ -81,6 +101,13 @@ public class MechShooting : MonoBehaviour
 
         }
         //print(timer);
-        
+
+    }
+
+    IEnumerator shooting(GameObject rocket_launcher)
+    {
+        yield return new WaitForSeconds(1);
+        rocket_launcher.GetComponent<Gun>().shoot();
+
     }
 }
