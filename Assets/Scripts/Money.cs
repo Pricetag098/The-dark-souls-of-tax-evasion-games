d6ebbc669685e3aa4772927cf47c5813ce3da6b6
Pricetag_ <jakesemail098@gmail.com>
2021-08-05 12:32:26 +0930

    Tax

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -5,7 +5,23 @@ using UnityEngine;
 public class Money : MonoBehaviour
 {
 
-	public static int money = 100000;
-    // Start is called before the first frame update
-    
+	public static int money = 0;
+    public int testVar;
+    public float incomeRate = 10;
+    public int   incomeAmount = 10;
+    float timer = 0;
+
+
+
+    public static int lvl = 1;
+    public void Update()
+    {
+        if(timer > incomeRate)
+        {
+            money += incomeAmount * lvl;
+        }
+        timer += Time.deltaTime;
+        testVar = money;
+    }
+
 }
